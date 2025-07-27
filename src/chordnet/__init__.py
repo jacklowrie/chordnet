e8b52bbd89491a000a64799fdf1adbc976d6a8f4
Jack Lowrie <jacklowrie@me.com>
2025-07-26 20:14:09 -0500

    make sure _Net can be imported in tests

--- src/chordnet/__init__.py
+++ src/chordnet/__init__.py
@@ -2,8 +2,5 @@ from .node import Node
 from .address import Address
 from .net import _Net
 
-def hello() -> str:
-    return "Hello from chordnet!"
-
-__all__=['Node', 'Address', '_Net']
+__all__=['Node', 'Address']
 
