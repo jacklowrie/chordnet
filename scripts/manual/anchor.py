5b35c4492dbe1a4b9280021ea0572b9e94bfbd35
Jack Lowrie <jacklowrie@me.com>
2025-09-25 11:14:06 -0400

    add manual scripts. ignore bpython from typechecking

--- scripts/manual/anchor.py
+++ scripts/manual/anchor.py
@@ -1,1 +1,27 @@
 """anchor.py: creates and starts an anchor node for testing."""
+import sys
+
+import bpython
+
+from chordnet import Node as ChordNode
+
+
+def main() -> None:
+    """Creates a new ring with this computer as the only node."""
+    if len(sys.argv) != 3:
+        print("usage: [uv run] python anchor.py ip_addr port_no")
+        exit(1)
+
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+
+    node = ChordNode(ip, port)
+    # create (start) the ring
+    node.create()
+    print(f"Node created as \"node\": {node.address}", file=sys.stderr)
+    bpython.embed(locals_=locals())
+    node.stop()
+
+
+if __name__ == '__main__':
+    main()
--- scripts/manual/joiner.py
+++ scripts/manual/joiner.py
@@ -1,1 +1,30 @@
 """joiner.py: creates a joining node for debugging."""
+import sys
+
+import bpython
+
+from chordnet import Node as ChordNode
+
+
+def main() -> None:
+    """Creates a new ring with this computer as the only node."""
+    if len(sys.argv) != 3:
+        print("usage: [uv run] python " \
+              "joiner.py this_ip this_port target_ip target_port")
+        exit(1)
+
+        # Get IP and port from command line arguments
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+    target_ip = sys.argv[3]
+    target_port = int(sys.argv[4])
+
+    # Create and join node
+    node = ChordNode(ip, port)
+    node.join(target_ip, target_port)
+    bpython.embed(locals_=locals())
+    node.stop()
+
+
+if __name__ == '__main__':
+    main()
