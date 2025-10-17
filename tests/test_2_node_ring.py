3a9991940b87cc8fe99b3ffeff2e536d8b9db5e2
Jack Lowrie <jacklowrie@me.com>
2025-10-17 14:36:19 -0400

    start test file

---
+++ tests/test_2_node_ring.py
@@ -0,0 +1,13 @@
+"""test_node.py: tests for the Node class.
+
+NOTE: many of these tests were AI generated as regression tests, in preparation
+for refactoring.
+"""
+from loguru import logger
+
+logger.enable("chordnet")
+
+# Global test variables
+ip: str = "127.0.0.1"
+anchor_port: int = 1234
+joiner_port: int = 2345
